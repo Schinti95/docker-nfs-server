32a0b68953d9d7636b2905133957c86ada3e5f17
Stefan Schintler <stefan.schintler@omnity.at>
2016-09-01 22:05:02 +0200

    added feature to add specific hosts

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
 set -e
+declare -a hosts=('*');
+if [ -n "$ALLOWED_HOSTS" ];
+	then 
+	unset hosts;
+	IFS=';' read -r -a hosts <<< "$ALLOWED_HOSTS"
+	echo "Specific Hosts enabled!"; 
+
+	else 
+	echo "Wildcard Hosts enabled!"; 
+fi
 
 mounts="${@}"
 
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
-  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+  echo -n "$src " >> /etc/exports
+  for host in "${hosts[@]}"; do
+    echo -n "$host(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+  done
+  echo "" >> /etc/exports
 done
 
 exec runsvdir /etc/sv
